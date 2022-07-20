d0babee6959c963b24ce5c013d960ee02abe5e94
Marijan Kelava <marijankelava2@gmail.com>
2022-07-20 21:06:00 +0200

    Remove logic to meal service & cleanup

--- src/Controller/MealsController.php
+++ src/Controller/MealsController.php
@@ -77,37 +77,9 @@ class MealsController extends AbstractController
         
         $parameters = $request->query->all();
 
-        $with = [];
-        if (isset($parameters['with'])){
-        $with = explode(',', $parameters['with']);
-        }
-        $data = $this->mealService->getMeals();
+        $data = $this->mealService->getMeals($parameters);
 
-        /*
-        $meals = $this->mealRepository->getMealsByCriteria($parameters, $with);
-
-        $data = [];
-        foreach ($meals as $key => $meal) {
-            $data['data'][$key]['id'] = $meal['id'];    
-            $data['data'][$key]['title'] = $meal['title'];    
-            $data['data'][$key]['description'] = $meal['description']; 
-            $data['data'][$key]['category']['id'] = $meal[0]['category'][0]['id'];  
-            $data['data'][$key]['category']['title'] = $meal[0]['category'][0]['contents'][0]['title'];  
-            $data['data'][$key]['category']['slug'] = $meal[0]['category'][0]['slug']; 
-       
-            echo '<pre>';
-            print_r($data);
-            echo '</pre>';
-            die;
-
-        }
-        die;
-        dd($meals);
-*/
-        return $this->json([
-            //'categories' => $categories,
-            $data
-        ]);
+        return $this->json($data);
     }
 
     /**
--- src/Services/MealService.php
+++ src/Services/MealService.php
@@ -2,10 +2,39 @@
 
 namespace App\Services;
 
+use App\Repository\MealRepository;
+
 final class MealService 
 {
-    public function getMeals()
+
+    private $mealRepository;
+
+    public function __construct(
+        MealRepository $mealRepository
+        )
     {
-        return 'meals';
+        $this->mealRepository = $mealRepository;
+    } 
+
+    public function getMeals(array $parameters)
+    {
+        $with = [];
+        
+        if (isset($parameters['with'])){
+        $with = explode(',', $parameters['with']);
+        }
+
+        $meals = $this->mealRepository->getMealsByCriteria($parameters, $with);
+
+        $data = [];
+        foreach ($meals as $key => $meal) {
+            $data['data'][$key]['id'] = $meal['id'];    
+            $data['data'][$key]['title'] = $meal['title'];    
+            $data['data'][$key]['description'] = $meal['description']; 
+            $data['data'][$key]['category']['id'] = $meal[0]['category'][0]['id'];  
+            $data['data'][$key]['category']['title'] = $meal[0]['category'][0]['contents'][0]['title'];  
+            $data['data'][$key]['category']['slug'] = $meal[0]['category'][0]['slug']; 
+        }
+        return $data;
     }
 }
