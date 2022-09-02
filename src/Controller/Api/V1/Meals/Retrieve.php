1ebe62786ca1b9297d7c473ca54a7a277040a3a9
Marijan Kelava <marijankelava2@gmail.com>
2022-09-02 19:48:43 +0200

    Refactor meals controller

--- src/Controller/Api/V1/Meals/Retrieve.php
+++ src/Controller/Api/V1/Meals/Retrieve.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Api\V1\Meals;
 
 use App\Services\MealService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -9,7 +9,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
-final class MealsController extends AbstractController
+final class Retrieve extends AbstractController
 {
     private MealService $mealService;
 
